79de20ba2f1b161aa0e696196f70bb8441014b34
payal <payal@aum.bz>
2020-06-08 10:15:49 +0530

    encrypt id pass define

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,6 @@ define("DB_HOST_NAME", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "payalt40");
 define("DB_DATABASE_NAME", "project");
+define("ENCRYPTION_IV", '1234567891011121');
+define("ENCRYPTION_KEY", 'PayalTalaviya');
 
--- models/Users.php
+++ models/Users.php
@@ -13,23 +13,14 @@ class Users extends database {
 		return $data;
 	}
 	public function encrypt_fun($simple_string){
-		$ciphering = "AES-128-CTR";
-		$iv_length = openssl_cipher_iv_length($ciphering); 
-		$options = 0;  
-		$encryption_iv = '1234567891011121';  
-		$encryption_key = "PayalTalaviya"; 
-		$encryption = openssl_encrypt($simple_string, $ciphering, 
-            $encryption_key, $options, $encryption_iv);
+		$encryption = openssl_encrypt($simple_string, 'AES-128-CTR', 
+         ENCRYPTION_KEY, 0, ENCRYPTION_IV);
 		return $encryption;
 	}
 	public function decrypt_fun($encryption){
-		$ciphering = "AES-128-CTR";
-		$iv_length = openssl_cipher_iv_length($ciphering); 
-		$options = 0; 
-		$decryption_iv = '1234567891011121'; 
-		$decryption_key = "PayalTalaviya";
-		$decryption=openssl_decrypt ($encryption, $ciphering,  
-        $decryption_key, $options, $decryption_iv);
+		
+		$decryption=openssl_decrypt ($encryption, 'AES-128-CTR',  
+        ENCRYPTION_KEY, 0, ENCRYPTION_IV);
 		return $decryption;
 	}
 }
