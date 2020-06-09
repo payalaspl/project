2da392a4e9590c22b3a964ce3e6d0d1ee95cd2f0
payal <payal@aum.bz>
2020-06-09 12:31:31 +0530

    paging

--- categories.php
+++ categories.php
@@ -7,9 +7,20 @@ require_once __DIR__ . "/models/Categories.php";
 
 // get latest posts from DB
 if(isset($_SESSION['admin_login']) && $_SESSION['admin_login'] != ""){
+
+	$limit = 2;  // Number of entries to show in a page. 
+    // Look for a GET variable page if not found default is 1.      
+    if (isset($_GET["page"])) {  
+      $pn  = $_GET["page"];  
+    }  
+    else {  
+      $pn=1;  
+    };   
+  
+    $start_from = ($pn-1) * $limit;  
 	$objCat = new Categories();
-	$cat = $objCat->select("SELECT * FROM categories ORDER BY  id DESC ");
-	 // print_r($posts);exit();
-	// render the template
-	echo $twig->render('categories.html.twig', array('categories' => $cat));
+	$cat = $objCat->select("SELECT * FROM categories  LIMIT $start_from, $limit  ");
+	$total = $objCat->select("SELECT * FROM categories");
+	 $total_pages = ceil(count($total) / $limit); 
+	echo $twig->render('categories.html.twig', array('categories' => $cat,'count' => $total_pages));
 }
